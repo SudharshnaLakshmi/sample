e0d334350c5d848e544d8e33343594b603241aec
Sudharshna Lakshmi.S <sudharshnalakshmi@gmail.com>
2024-08-12 14:50:51 +0530

    update

--- terraform-iis-setup/main.tf
+++ terraform-iis-setup/main.tf
@@ -8,3 +8,28 @@ terraform {
 }
 
 provider "local" {}
+
+# Provisioning a Windows VM (Assuming you are using an existing Windows VM)
+resource "local_file" "install_iis" {
+  content = <<-EOF
+  Install-WindowsFeature -name Web-Server -IncludeManagementTools
+  EOF
+  filename = "install_iis.ps1"
+}
+
+resource "null_resource" "run_install_iis" {
+  provisioner "local-exec" {
+    command = "powershell.exe -ExecutionPolicy Bypass -File install_iis.ps1"
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+# Start IIS Server
+resource "null_resource" "start_iis" {
+  provisioner "local-exec" {
+    command = "iisreset /start"
+  }
+  depends_on = [null_resource.run_install_iis]
+}
